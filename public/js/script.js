91e767cd41f2c5ac2d0c0f5adaf1c626e250274c
Stephen Polley <stephenHpolley@gmail.com>
2021-01-22 11:20:39 +0000

    Integrated Ghana Post

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,9 @@ $(function () {
 
     const addressTypes = document.querySelectorAll(".address-type");
     const formContents = document.querySelectorAll(".form-content")
+
+    const errorBox = document.getElementById("error-box");
+    const errorMessage = document.querySelector("#error-box small")
     if (addressTypes) {
         addressTypes.forEach(function (addressType) {
             addressType.addEventListener("click", function (event) {
@@ -35,6 +38,7 @@ $(function () {
     if (latlongForm) {
         latlongForm.addEventListener("submit", function (event) {
             event.preventDefault();
+            errorBox.style.display="none";
             const lat = document.getElementById("lat").value;
             const long = document.getElementById("long").value;
             const postData = {
@@ -84,7 +88,7 @@ $(function () {
                         const table_Tbody = document.getElementById("tbody");
                         table_Tbody.innerHTML = tableBody;
 
-                    } else {
+                    } else if (data.status){
                         let tableBody = "";
                         for (let i = 0; i < 5; i++) {
                             let values
@@ -119,15 +123,153 @@ $(function () {
                         table_Tbody.innerHTML = tableBody;
 
 
+                    }else if (data.error){
+                        errorMessage.innerHTML =data.message.toString();
+                        errorBox.style.display="block";
+                        document.getElementById("tbody").innerHTML="";
                     }
 
 
                 }).fail(function (error) {
+                errorMessage.innerHTML ="No Network. Please check internet connection and try again";
+                errorBox.style.display="block";
+                document.getElementById("tbody").innerHTML="";
+
+
 
 
 
             })
 
+
+        })
+    }
+
+    const ghpostForm = document.getElementById("gh-post-form");
+    if (ghpostForm){
+        const progressIndicator = document.getElementById("progressIndicator")
+        ghpostForm.addEventListener("submit", function (event) {
+            event.preventDefault();
+            progressIndicator.style.display="block";
+            errorBox.style.display="none";
+            const ghpost = document.getElementById("ghanapost-code").value;
+            const postData = {
+                type: 'ghanapost',
+                ghpost
+
+            };
+
+            $.post("/api/network", postData)
+                .done(function (data) {
+                    progressIndicator.style.display="none"
+
+                    const location = data.loc;
+                    const networkStatus =data.netstatus;
+                    const area = data.area
+
+                    if (data.status === 0) {
+                        const site_info = data.site;
+                        let tableBody = "";
+                        for (let i = 0; i < 5; i++) {
+                            let values
+                            switch (i) {
+                                case 0:
+                                    values = `<td>Location</td><td>${location}</td>`;
+                                    break;
+
+                                case 1:
+                                    values = `<td>Area</td><td>${area}</td>`;
+                                    break;
+
+                                case 2:
+                                    values = `<td>Network Coverage</td><td>${networkStatus}</td>`;
+                                    break;
+
+                                case 3:
+                                    values = `<td>Serving Site Name</td><td>${site_info.name}</td>`;
+                                    break;
+                                case 4:
+                                    values = `<td>Serving Site Id</td><td>${site_info.site_id}</td>`;
+                                    break;
+
+                                case 5:
+                                    values = `<td>Serving Site Status</td><td>${site_info.status === 'online' ? 'In-Service' : 'Out-Of-Service'}</td>`;
+                                    break;
+
+
+                            }
+
+                            tableBody += `<tr>${values}</tr>`
+
+
+                        }
+
+                        const table_Tbody = document.getElementById("tbody");
+                        table_Tbody.innerHTML = tableBody;
+
+                    } else if (data.status) {
+                        let tableBody = "";
+                        for (let i = 0; i < 5; i++) {
+                            let values
+                            switch (i) {
+                                case 0:
+                                    values = `<td>Location</td><td>${location}</td>`;
+                                    break;
+                                case 1:
+                                    values = `<td>Network Coverage</td><td>POOR</td>`;
+                                    break;
+
+                                case 2:
+                                    values = `<td>Serving Site Name</td><td></td>`;
+                                    break;
+                                case 3:
+                                    values = `<td>Serving Site Id</td><td></td>`;
+                                    break;
+
+                                case 4:
+                                    values = `<td>Serving Site Status</td><td></td>`;
+                                    break;
+
+
+                            }
+
+                            tableBody += `<tr>${values}</tr>`;
+
+
+                        }
+
+                        const table_Tbody = document.getElementById("tbody");
+                        table_Tbody.innerHTML = tableBody;
+
+
+                    }else if (data.error) {
+                        errorMessage.innerHTML =data.message.toString();
+                        errorBox.style.display="block";
+                        document.getElementById("tbody").innerHTML="";
+
+
+                    }
+
+
+                }).fail(function (error) {
+                    errorMessage.innerHTML ="No Network. Please check internet connection and try again";
+                    errorBox.style.display="block";
+                document.getElementById("tbody").innerHTML="";
+
+
+
+            })
+
+
+        })
+
+    }
+
+    const timeButtonError = document.getElementById("times-button-error");
+    if (timeButtonError) {
+        timeButtonError.addEventListener("click", function (event) {
+            const wrapper = timeButtonError.closest("div");
+            wrapper.style.display = "none";
 
         })
     }
--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 
 const router = express.Router();
 const Sites = require("./models/geodata")
@@ -11,52 +12,115 @@ router.get("/", async (req, res) => {
 
 
 router.post("/api/network", async (req, res) => {
-    console.log(req.body)
-    const {type, ghpost, lat, long} = req.body;
-    let latitude, longitude;
-    if (type === "ghanapost") {
-        console.log(ghpost)
+ try {
+     let {type, ghpost, lat, long} = req.body;
+     let latitude, longitude;
+     if (type === "ghanapost") {
+         ghpost = ghpost.toString().trim().replace(/[-\s]/g,"").toUpperCase();
+         console.log(ghpost)
+         const url ="https://api.ghanapostgps.com/v2/PublicGPGPSAPI.aspx"
+         axios.post(url,null, {
+             auth:{
+                 username:"005056986c49",
+                 password:"U3VyZmxpbmVHSEBTdXJmbGluZSBDb21tdW5pY2F0aW9ucyBMaW1pdGVk"
+             },
 
-    } else {
-        latitude = lat;
-        longitude = long;
-    }
+             headers:{
+                 DeviceID:"005056986c49"
+             },
+             params:{
+                 Action:"GetLocation",
+                 GPSName:ghpost
+             }
+         }).then(response =>{
+             if (response.data){
+                 const {CenterLatitude:lat, CenterLongitude:long, Area:area} = response.data.Table[0];
+
+                 latitude =parseFloat(lat).toFixed(7);
+                 longitude =parseFloat(long).toFixed(7);
+                 getSiteDetails(latitude, longitude,res, area)
+
+             }else {
+                 res.json({
+                     error:"error",
+                     message:`Invalid Ghana Post Code ${ghpost}`
+                 })
+
+             }
 
 
-    latitude = parseFloat(latitude);
-    longitude = parseFloat(longitude);
 
-    let [d500, d700,d1000,d1200]=[500,700,1000,1200]
 
+
+         }).catch(error =>{
+             console.log(error);
+             res.json({
+                 error:"error",
+                 message:`System Failure: Error connecting to GhanaPost Service`
+             })
+         })
+
+
+     } else {
+         latitude = parseFloat(lat);
+         longitude = parseFloat(long);
+         await getSiteDetails(latitude, longitude, res)
+
+     }
+
+ }catch (error){
+     console.log(error);
+     res.json({
+         error:"error",
+         message:`System Failure: Please contact SysAdmin`
+     })
+
+ }
+
+
+
+
+
+
+
+
+})
+
+
+
+
+async function getSiteDetails(latitude, longitude, res, area) {
+    let [d500, d700, d1000, d1200] = [500, 700, 1000, 1200]
 
 
     let site = await Sites.findOne({
         location: {$nearSphere: {$geometry: {type: 'Point', coordinates: [longitude, latitude]}, $maxDistance: d500}},
 
-    },['name', 'site_id', 'status'])
+    }, ['name', 'site_id', 'status'])
 
-    if (site){
-       return  res.json({
-            loc:`${lat}, ${long}`,
-           netstatus:"EXCELLENT",
-            status :0,
-            message:"success",
+    if (site) {
+        return res.json({
+            loc: `${latitude}, ${longitude}`,
+            area,
+            netstatus: "EXCELLENT",
+            status: 0,
+            message: "success",
             site
 
         })
     }
 
-
     site = await Sites.findOne({
         location: {$nearSphere: {$geometry: {type: 'Point', coordinates: [longitude, latitude]}, $maxDistance: d700}},
 
-    },['name', 'site_id', 'status']);
-    if (site){
-        return  res.json({
-            loc:`${lat}, ${long}`,
-            netstatus:"VERY GOOD",
-            status :0,
-            message:"success",
+    }, ['name', 'site_id', 'status']);
+    if (site) {
+        return res.json({
+            loc: `${latitude}, ${longitude}`,
+            area,
+            netstatus: "VERY GOOD",
+            status: 0,
+            message: "success",
             site
 
         })
@@ -66,30 +130,31 @@ router.post("/api/network", async (req, res) => {
     site = await Sites.findOne({
         location: {$nearSphere: {$geometry: {type: 'Point', coordinates: [longitude, latitude]}, $maxDistance: d1000}},
 
-    },['name', 'site_id', 'status']);
-    if (site){
-        return  res.json({
-            loc:`${lat}, ${long}`,
-            netstatus:"GOOD",
-            status :0,
-            message:"success",
+    }, ['name', 'site_id', 'status']);
+    if (site) {
+        return res.json({
+            loc: `${latitude}, ${longitude}`,
+            area,
+            netstatus: "GOOD",
+            status: 0,
+            message: "success",
             site
 
         })
     }
 
 
-
     site = await Sites.findOne({
         location: {$nearSphere: {$geometry: {type: 'Point', coordinates: [longitude, latitude]}, $maxDistance: d1200}},
 
-    },['name', 'site_id', 'status']);
-    if (site){
-        return  res.json({
-            loc:`${lat}, ${long}`,
-            netstatus:"FAIR",
-            status :0,
-            message:"success",
+    }, ['name', 'site_id', 'status']);
+    if (site) {
+        return res.json({
+            loc: `${latitude}, ${longitude}`,
+            area,
+            netstatus: "FAIR",
+            status: 0,
+            message: "success",
             site
 
         })
@@ -98,30 +163,20 @@ router.post("/api/network", async (req, res) => {
     site = await Sites.findOne({
         location: {$nearSphere: {$geometry: {type: 'Point', coordinates: [longitude, latitude]}, $minDistance: 1300}},
 
-    },['name', 'site_id', 'status']);
-    if (site){
-        return  res.json({
-            loc:`${lat}, ${long}`,
-            netstatus:"POOR",
-            status :0,
-            message:"success",
+    }, ['name', 'site_id', 'status']);
+    if (site) {
+        return res.json({
+            loc: `${latitude}, ${longitude}`,
+            area,
+            netstatus: "POOR",
+            status: 0,
+            message: "success",
             site
 
         })
     }
 
-        res.json({
-            loc:`${lat}, ${long}`,
-           status:1,
-            message:"error",
-        })
-
-
-
-
-
-
-})
+}
 
 
 module.exports = router;
